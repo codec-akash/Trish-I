5a69a49f4d90b730cfcfe393399b1770c6907c47
Akash Dubey <akashrdubey19@gmail.com>
2021-09-03 19:25:56 +0530

    basic app structure added

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //TODO: Call Body part card here
+            Row(
+              children: [],
+            ),
+            Row(
+              children: [],
+            ),
+            Row(
+              children: [],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/body_part_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BodypartCard extends StatelessWidget {
+  const BodypartCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: Design body part card
+    return GestureDetector(
+      onTap: () {},
+      child: Container(),
+    );
+  }
+}
