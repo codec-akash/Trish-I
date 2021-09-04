7488977116eb4134ed12a62615c9aed6cf20b56d
rakhi2207 <jharakhi1211@gmail.com>
2021-09-04 09:12:23 +0530

    Resolved SplashScreen Issues

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(SplashScreen());
-}
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title:"App",
-      home:Scaffold(
+      return Scaffold(
           backgroundColor: Color(0xFF017e6F),
           body:Center(
               child: Image(
@@ -19,7 +14,6 @@ class SplashScreen extends StatelessWidget {
                 width: 300,
               ),
           )
-      )
-    );
+      );
   }
 }
