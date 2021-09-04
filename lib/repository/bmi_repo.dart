ace1344c5997ae3c57ce31fb0ce022ba759a02b6
Sanchit <sanchit02@outlook.com>
2021-09-04 10:45:42 +0530

    BMI formula fixed

--- lib/repository/bmi_repo.dart
+++ lib/repository/bmi_repo.dart
@@ -1,6 +1,6 @@
 class BmiRepo {
   double calculateBMI({required double height, required double weight}) {
-    double bmi = height / (weight * weight);
+    double bmi = weight / (height * height);
     return bmi;
   }
 }
