cbf1917072f09997f28b9f86be28521913a9b08c
Sanchit <sanchit02@outlook.com>
2021-09-29 21:33:46 +0530

    freq checking partially complete

--- lib/widgets/slider_widget.dart
+++ lib/widgets/slider_widget.dart
@@ -5,10 +5,70 @@ class SliderWidget extends StatefulWidget {
 
   @override
   _SliderWidgetState createState() => _SliderWidgetState();
+
+  void getBmi(double? bmi, int? age) {}
 }
 
 class _SliderWidgetState extends State<SliderWidget> {
-  int _value = 6;
+  int _value = 0;
+
+  int? freqCheck(String? bmiStatus, int? age) {
+    if (bmiStatus == 'Underweight') {
+      if ((age! >= 20) && (age <= 50)) {
+        return 40;
+      } else if ((age >= 51) && (age <= 60)) {
+        return 30;
+      } else if ((age >= 61) && (age <= 90)) {
+        return 20;
+      }
+    } else if (bmiStatus == 'Normal') {
+      if ((age! >= 20) && (age <= 50)) {
+        return 50;
+      } else if ((age >= 51) && (age <= 60)) {
+        return 40;
+      } else if ((age >= 61) && (age <= 90)) {
+        return 30;
+      }
+    } else if (bmiStatus == 'Overweight') {
+      if ((age! >= 20) && (age <= 60)) {
+        return 50;
+      } else if ((age >= 61) && (age <= 70)) {
+        return 40;
+      } else if ((age >= 71) && (age <= 90)) {
+        return 30;
+      }
+    } else if (bmiStatus == 'Obese') {
+      if ((age! >= 20) && (age <= 60)) {
+        return 60;
+      } else if ((age >= 61) && (age <= 70)) {
+        return 50;
+      } else if ((age >= 71) && (age <= 80)) {
+        return 40;
+      } else if ((age >= 81) && (age <= 90)) {
+        return 30;
+      }
+    }
+  }
+
+  void getBmi(double? bmiValue, int? age) {
+    print(bmiValue);
+    print(age);
+    String? bmiStatus;
+    if (bmiValue! < 18.5) {
+      print('Underweight');
+      bmiStatus = 'Underweight';
+    } else if (bmiValue >= 18.5 && bmiValue < 25) {
+      print('Normal');
+      bmiStatus = 'Normal';
+    } else if (bmiValue >= 25 && bmiValue < 30) {
+      print('Overweight');
+      bmiStatus = 'Overweight';
+    } else if (bmiValue >= 30) {
+      print('Obese');
+      bmiStatus = 'Obese';
+    }
+    print('Freq: ${freqCheck(bmiStatus, age)}');
+  }
 
   @override
   Widget build(BuildContext context) {
